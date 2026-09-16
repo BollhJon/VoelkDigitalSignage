eb1055a50fe44c3f772eb12f0aa1e4cc245766d8
Jonas Bollhalder <43448146+BollhJon@users.noreply.github.com>
2026-09-16 16:51:51 +0200

    Update clear_envoirment.sh

--- scripts/clear_envoirment.sh
+++ scripts/clear_envoirment.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 ENV_FILE="/etc/environment"
 SERVICE_ENV_FILE="/etc/voelk-signage.env"
 
-rm -f "$ENV_FILE*"
-rm -f "$SERVICE_ENV_FILE*"
+sudo rm -f "$ENV_FILE" "$ENV_FILE".backup.*
+sudo rm -f "$SERVICE_ENV_FILE" "$SERVICE_ENV_FILE".backup.*
