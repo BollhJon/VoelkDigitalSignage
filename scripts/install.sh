fdc26b00a9421f2bcb1159b0d8d2bb603d179309
Jonas Bollhalder <43448146+BollhJon@users.noreply.github.com>
2026-09-16 13:41:34 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,8 +15,9 @@ sudo apt-get install -y git python3 python3-venv chromium-browser
 
 echo "Setup Virtual Enviroment and install/update pyhton packages"
 python3 -m venv "$INSTALL_DIR/.venv"
-"$INSTALL_DIR/.venv/bin/pip" install --upgrade pip
-"$INSTALL_DIR/.venv/bin/pip" install -r "$INSTALL_DIR/requirements.txt"
+source "$INSTALL_DIR/.venv/bin/activate"
+pip install --upgrade pip
+pip install -r "$INSTALL_DIR/requirements.txt"
 
 echo "Setup Server Service"
 sudo install -m 644 "$INSTALL_DIR/deploy/signage.service" /etc/systemd/system/signage.service
